3f0d73e25df9078003ecc95639322065a985b43d
SarinSwift <sarinyaswift@gmail.com>
2018-12-13 12:33:27 -0800

    auotolayout for views

---
+++ AutoLayoutProg/AutoLayoutProg/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  AutoLayoutProg
+//
+//  Created by Sarin Swift on 12/12/18.
+//  Copyright © 2018 sarinswift. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
+
+class ViewController: UIViewController {
+    
+    let bearImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        // this enables autolayout for our imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        attributedText.append(NSMutableAttributedString(string: "\n\n\nAre you ready for tons and tons of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        textView.attributedText = attributedText
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    // encapsulation in this class so outside classes can not interact with the private things inside this class
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+        return button
+    }()
+    // bottom pageControl
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .mainPink
+        pc.pageIndicatorTintColor = #colorLiteral(red: 0.9764705882, green: 0.8117647059, blue: 0.8784313725, alpha: 1)
+        return pc
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(descriptionTextView)
+        
+        setUpBottomControls()
+        setUpLayout()
+    }
+
+    // private is only accessible only within the scope of this class
+    private func setUpLayout() {
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        // is the invisible box that covers the top half of the view
+        // this works well when rotated
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        // add the imageView inside the top half invisible view
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        // setting the description to be right under the center of screen
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    // fileprivate can be accessed outside of this class, as long as its in the same source file
+    fileprivate func setUpBottomControls() {
+        
+        let bottomControlStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlStackView.distribution = .fillEqually
+        view.addSubview(bottomControlStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlStackView.heightAnchor.constraint(equalToConstant: 50),
+            ])
+    }
+
+}
+
