76eb8a38c7c6639cc82290c8d54e3e6676cb76a7
SarinSwift <sarinyaswift@gmail.com>
2019-10-09 20:46:54 -0700

    set up page for detailed cell, notes on swiftui view object

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -13,10 +13,8 @@ struct ContentView: View {
     var rooms: [Room] = testData
     
     var body: some View {
-        // Wraping the list in a navigation controller
         NavigationView {
             List(rooms) { room in
-                // Makes the cell selectable and pushes to a new page
                 RoomCell(room: room)
             }
             .navigationBarTitle("Rooms")
@@ -27,17 +25,15 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        // NOTE: this testData will only be showed on the side canvas
         ContentView(rooms: testData)
     }
 }
 
 struct RoomCell: View {
-    // NOTE: variables declared here must be passed upon instantiating this RoomCell struct
     var room: Room
     
     var body: some View {
-        return NavigationLink(destination: Text(room.name)) {
+        return NavigationLink(destination: RoomDetail(room: room)) {
             Image(systemName: "photo")
                 .cornerRadius(3)
             
--- SwiftUIPractice/SwiftUIPractice/RoomDetail.swift
+++ SwiftUIPractice/SwiftUIPractice/RoomDetail.swift
@@ -8,16 +8,31 @@
 
 import SwiftUI
 
+/*
+ Notes: The View type in SwiftUI is a struct that conforms to the View Protocol rather than a class (ex. UIView). This means that the View does not inherit any stored property -- allocated on the Stack -- and passed by value.
+ RoomDetail stores the only property: room of type Room. Meaning that this struct will only have the size of a Room object!
+ 
+ Views in SwiftUI are incredibly light weight. Extractign a subview has no runtime ahead.
+ The View protocol only has a single requirement: body. The renderign of any view you build is just the rendering of it's body. And in SwiftUI a view defines a set of dependencies
+ 
+ State variables - source of truth. Because SwiftUI can tell when state variables are read and written. So when the state changes it knows which state to render. This rendering knows when to refresh by asking for a new body
+*/
+
 struct RoomDetail: View {
-    let room: Room = testData[0]
+    let room: Room
+    @State private var zoomed = false
     
     var body: some View {
         Image(room.imageName)
+            .resizable()
+            .aspectRatio(contentMode: zoomed ? .fill : .fit)
+            // inline sets the title to look likee what it would normally look like on a navigation controller bar
+            .navigationBarTitle(Text(room.name), displayMode: .inline)
     }
 }
 
 struct RoomDetail_Previews: PreviewProvider {
     static var previews: some View {
-        RoomDetail()
+        RoomDetail(room: testData[0])
     }
 }
